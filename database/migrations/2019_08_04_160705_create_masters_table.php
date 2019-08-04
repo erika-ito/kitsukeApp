c90bce396791a08ab0bf0d011029297f76711383
Erika Ito <itouerika21@gmail.com>
2019-08-04 19:26:26 +0900

    追加：　講師データをシーダーで挿入
    講師の優先度（rank）のみ変更、他項目は同じ内容を5件挿入した。

---
+++ database/migrations/2019_08_04_160705_create_masters_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMastersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('masters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('rank');
+            $table->string('name');
+            $table->string('furigana');
+            $table->string('zip_code');
+            $table->string('address');
+            $table->string('home_phone')->nullable();
+            $table->string('cell_phone')->nullable();
+            $table->string('mail')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('masters');
+    }
+}
