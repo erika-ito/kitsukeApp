5d91f74a4f150fc2f8f215a007cd2336be7386fc
Erika Ito <itouerika21@gmail.com>
2019-08-10 18:23:25 +0900

    改善：　seederにfakerとfactoryを使用
    シーダーファイルに直接記述していたダミーデータを、
    フェイカーとファクトリーを使用した挿入方法に変更。
    $faker->kanaNameでの挿入データは、カタカナ、
    かつ漢字の氏名とは一致しない。

--- database/seeds/MastersTableSeeder.php
+++ database/seeds/MastersTableSeeder.php
@@ -13,20 +13,6 @@ class MastersTableSeeder extends Seeder
      */
     public function run()
     {
-        $ranks = ['1','2','3','4','5'];
-
-        foreach ($ranks as $rank) {
-            DB::table('masters')->insert([
-                'rank' => $rank,
-                'name' => '山田花子',
-                'furigana' => 'やまだはなこ',
-                'zip_code' => '111-1111',
-                'address' => '東京都新宿区111-111',
-                'home_phone' => '03-0000-0000',
-                'mail' => 'abc@gmail.com',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ]);
-        }
+        factory(App\Master::class, 10)->create();
     }
 }
