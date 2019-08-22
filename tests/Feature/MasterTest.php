3a68d2b45772c09ef2016fd312588a6bd441a34a
Erika Ito <itouerika21@gmail.com>
2019-08-22 14:32:37 +0900

    追加：　講師一覧の検索表示のテストが完成

--- tests/Feature/MasterTest.php
+++ tests/Feature/MasterTest.php
@@ -19,86 +19,149 @@ class MasterTest extends TestCase
         $this->seed('MasterTestSeeder');
     }
 
-    // 検索一致の場合
-    public function testMatch()
+    // 氏名（漢字）で検索した場合、条件に部分一致する講師データが取得できること
+    public function testSearchKanjiGetPartialMatchMasters()
     {
-        $keyword = urlencode('いし');
-
-        $expected = [
-            [
-                'rank' => '5',
-                'name' => '石橋',
-                'furigana' => 'いしばし',
-                'zip_code' => '111-1111',
-                'address' => '東京都新宿区111-111',
-                'home_phone' => '03-0000-0000',
-                'mail' => 'abc@gmail.com',
-                // 'created_at' => Carbon::now(),
-                // 'updated_at' => Carbon::now(),
-            ],
-            [
-                'rank' => '5',
-                'name' => '石井',
-                'furigana' => 'いしい',
-                'zip_code' => '111-1111',
-                'address' => '東京都新宿区111-111',
-                'home_phone' => '03-0000-0000',
-                'mail' => 'abc@gmail.com',
-                // 'created_at' => Carbon::now(),
-                // 'updated_at' => Carbon::now(),
-            ],
-        ];
+        $keyword = urlencode('石');
         
-        $response = $this->get('/kitsuke/masters?keyword=$keyword');
+        $response = $this->get("/kitsuke/masters?keyword=$keyword");
 
         $response->assertStatus(200);
         $response->assertViewIs('masters.index');
-        $response->assertViewHas('masters', $expected);
+        $response->assertViewHas('masters', function($masters) {
+            // ページネーション
+            $this->assertEquals(2, $masters->total());
+            $this->assertEquals(1, $masters->currentPage());
+            $this->assertEquals(1, $masters->lastPage());
+            $this->assertCount(2, $masters);
+
+            // データ検証
+            $actual1 = $masters->get(0);
+            $this->assertEquals('5', $actual1->rank);
+            $this->assertEquals('石井', $actual1->name);
+            $this->assertEquals('いしい', $actual1->furigana);
+            $this->assertEquals('111-1111', $actual1->zip_code);
+            $this->assertEquals('東京都新宿区111-111', $actual1->address);
+            $this->assertEquals('03-0000-0000', $actual1->home_phone);
+            $this->assertEquals('abc@gmail.com', $actual1->mail);
+
+            $actual2 = $masters->get(1);
+            $this->assertEquals('5', $actual2->rank);
+            $this->assertEquals('石橋', $actual2->name);
+            $this->assertEquals('いしばし', $actual2->furigana);
+            $this->assertEquals('111-1111', $actual2->zip_code);
+            $this->assertEquals('東京都新宿区111-111', $actual2->address);
+            $this->assertEquals('03-0000-0000', $actual2->home_phone);
+            $this->assertEquals('abc@gmail.com', $actual2->mail);
+            
+            return true;
+        });
+    }
+    
+    // ふりがなで検索した場合、条件に部分一致する講師データが取得できること
+    public function testSearchFuriganaGetPartialMatchMasters()
+    {
+        $keyword = urlencode('いし');
+        
+        $response = $this->get("/kitsuke/masters?keyword=$keyword");
+
+        $response->assertStatus(200);
+        $response->assertViewIs('masters.index');
+        $response->assertViewHas('masters', function($masters) {
+            // ページネーション
+            $this->assertEquals(2, $masters->total());
+            $this->assertEquals(1, $masters->currentPage());
+            $this->assertEquals(1, $masters->lastPage());
+            $this->assertCount(2, $masters);
+
+            // データ検証
+            $actual1 = $masters->get(0);
+            $this->assertEquals('5', $actual1->rank);
+            $this->assertEquals('石井', $actual1->name);
+            $this->assertEquals('いしい', $actual1->furigana);
+            $this->assertEquals('111-1111', $actual1->zip_code);
+            $this->assertEquals('東京都新宿区111-111', $actual1->address);
+            $this->assertEquals('03-0000-0000', $actual1->home_phone);
+            $this->assertEquals('abc@gmail.com', $actual1->mail);
+
+            $actual2 = $masters->get(1);
+            $this->assertEquals('5', $actual2->rank);
+            $this->assertEquals('石橋', $actual2->name);
+            $this->assertEquals('いしばし', $actual2->furigana);
+            $this->assertEquals('111-1111', $actual2->zip_code);
+            $this->assertEquals('東京都新宿区111-111', $actual2->address);
+            $this->assertEquals('03-0000-0000', $actual2->home_phone);
+            $this->assertEquals('abc@gmail.com', $actual2->mail);
+            
+            return true;
+        });
     }
 
-    // 検索不一致の場合
-    // public function testNotMatch()
-    // {
-    //     $response = $this->get('/kitsuke/masters?keyword=あべ');
+    // 検索条件と合致するデータにない場合、講師データの取得がないこと
+    public function testSearchNotMatchKeywordGetNoMasters()
+    {
+        $keyword = urlencode('あべ');
+        
+        $response = $this->get("/kitsuke/masters?keyword=$keyword");
 
-    //     $response->assertViewHas('masters', function($masters) {
-    //         return $masters->isEmpty();
-    //     });
-    // }
+        $response->assertStatus(200);
+        $response->assertViewIs('masters.index');
+        $response->assertViewHas('masters', function($masters) {
+            // ページネーション
+            $this->assertEquals(0, $masters->total());
+            $this->assertEquals(1, $masters->currentPage());
+            $this->assertEquals(1, $masters->lastPage());
+            $this->assertCount(0, $masters);
+            
+            return true;
+        });
+    }
+    
+    // 検索条件がない場合、講師データが全件取得されること
+    public function testSearchNoKeywordGetPartialMatchMasters()
+    {
+        $keyword = urlencode('');
+        
+        $response = $this->get("/kitsuke/masters?keyword=$keyword");
 
-    // 検索キーワードがない場合
-    // public function testNoKeyword()
-    // {
-    //     $response = $this->get('/kitsuke/masters');
+        $response->assertStatus(200);
+        $response->assertViewIs('masters.index');
+        $response->assertViewHas('masters', function($masters) {
+            // ページネーション
+            $this->assertEquals(3, $masters->total());
+            $this->assertEquals(1, $masters->currentPage());
+            $this->assertEquals(1, $masters->lastPage());
+            $this->assertCount(3, $masters);
 
-    //     $response->assertViewHas('masters', function($masters) {
-    //         return $masters->contains('name', '伊藤');
-    //     });
-    // }
+            // データ検証
+            $actual1 = $masters->get(0);
+            $this->assertEquals('5', $actual1->rank);
+            $this->assertEquals('石井', $actual1->name);
+            $this->assertEquals('いしい', $actual1->furigana);
+            $this->assertEquals('111-1111', $actual1->zip_code);
+            $this->assertEquals('東京都新宿区111-111', $actual1->address);
+            $this->assertEquals('03-0000-0000', $actual1->home_phone);
+            $this->assertEquals('abc@gmail.com', $actual1->mail);
 
-    // public function testPractice()
-    // {
-        // $collection = collect([
-        //     [
-        //         'id' => 1,
-        //         'name' => 'たろう',
-        //     ],
-        //     [
-        //         'id' => 2,
-        //         'name' => 'じろう',
-        //     ],
-        //     [
-        //         'id' => 3,
-        //         'name' => 'さぶろう',
-        //     ],
-        // ]);
+            $actual2 = $masters->get(1);
+            $this->assertEquals('5', $actual2->rank);
+            $this->assertEquals('石橋', $actual2->name);
+            $this->assertEquals('いしばし', $actual2->furigana);
+            $this->assertEquals('111-1111', $actual2->zip_code);
+            $this->assertEquals('東京都新宿区111-111', $actual2->address);
+            $this->assertEquals('03-0000-0000', $actual2->home_phone);
+            $this->assertEquals('abc@gmail.com', $actual2->mail);
 
-        //     echo $collection->contains('name','さぶろう');
-
-        // $collection->each(function ($item) {
-        //     echo $item['name'];
-        // });
-
-        // $this->assertTrue(true);
-    // }
+            $actual3 = $masters->get(2);
+            $this->assertEquals('5', $actual3->rank);
+            $this->assertEquals('伊藤', $actual3->name);
+            $this->assertEquals('いとう', $actual3->furigana);
+            $this->assertEquals('111-1111', $actual3->zip_code);
+            $this->assertEquals('東京都新宿区111-111', $actual3->address);
+            $this->assertEquals('03-0000-0000', $actual3->home_phone);
+            $this->assertEquals('abc@gmail.com', $actual3->mail);
+            
+            return true;
+        });
+    }
 }
