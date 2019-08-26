59d156fb886b971fc72744a2911880f4a132737c
Erika Ito <itouerika21@gmail.com>
2019-08-27 00:01:26 +0900

    改善：　ナビバーの切り替え機能を追加

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,13 +19,16 @@
             
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto w-50 nav-justified">
-                    <li class="nav-item">
+                    <li class="nav-item {{ Request::is('kitsuke/reservations*') ? 'active' : '' }}">
+                    <!-- <li class="nav-item"> -->
                         <a class="nav-link" href="{{ route('reservations.index') }}">予約</span></a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item {{ Request::is('kitsuke/masters*') ? 'active' : '' }}">
+                    <!-- <li class="nav-item"> -->
                         <a class="nav-link" href="{{ route('masters.index') }}">講師</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item {{ Request::is('kitsuke/connectors*') ? 'active' : '' }}">
+                    <!-- <li class="nav-item"> -->
                         <a class="nav-link" href="{{ route('connectors.index') }}">連絡者</a>
                     </li>
                 </ul>
@@ -46,13 +49,13 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     @yield('scripts')
-    <script>
+    <!-- <script>
        $(function() {
            $('ul.navbar-nav li').click(function() {
                $('ul.navbar-nav li.active').removeClass('active');
                $(this).addClass('active');
            });
        });
-   </script>
+   </script> -->
 </body>
 </html>
