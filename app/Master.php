730b32d1ec28b5b5f719f57f618a799036c387c8
Erika Ito <itouerika21@gmail.com>
2019-08-30 23:13:47 +0900

    改善：　講師モデルの優先度のアクセサを変更

--- app/Master.php
+++ app/Master.php
@@ -35,23 +35,11 @@ class Master extends Model
     public function getFormattedRankAttribute()
     {
         switch($this->attributes['rank']){
-            case 5:
-                return 5;
-            
-            case 4:
-                return 4;
-
-            case 3:
-                return 3;
-            
-            case 2:
-                return 2;
-
-            case 1:
-                return 1;
-            
             case 0:
                 return '出張不可';
+            
+            default:
+                return $this->attributes['rank'];
         }
     }
 }
