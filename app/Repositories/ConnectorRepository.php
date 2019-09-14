c14cbc91169c9b7bd12b0b8ceb1428a522538296
Erika Ito <itouerika21@gmail.com>
2019-09-14 23:45:40 +0900

    修正：　ConnectorRepositoryのコメント部分の修正

--- app/Repositories/ConnectorRepository.php
+++ app/Repositories/ConnectorRepository.php
@@ -4,7 +4,7 @@ namespace App\Libs;
 
 class ConnectorRepository
 {
-    // 顧客テーブルの登録・更新
+    // 連絡者テーブルの登録・更新
     public static function fill($request, $match_connector)
     {
         $match_connector->fill($request->all());
