80c58de8d38adffe04c4666182bdacc094459f56
Erika Ito <itouerika21@gmail.com>
2019-09-20 17:46:58 +0900

    修正：　予約登録・編集処理にトランザクションを設定
    予約の登録の際、処理のどこかでエラーが起きると、不十分な内容で予約が1件成立してしまうため、
    一連の処理が成功したときのみ予約が成立するよう、トランザクションを設定した。
    編集処理も同様。

--- app/Services/ReservationService.php
+++ app/Services/ReservationService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use DB;
 use App\Master;
 use App\Reservation;
 use App\Repositories\ConnectorRepository;
@@ -28,65 +29,69 @@ class ReservationService {
     // 予約の新規登録
     public function create($request)
     {
-        $reservation = new Reservation();
+        DB::transaction(function() use($request) {
+            $reservation = new Reservation();
 
-        // 連絡者の検索と登録・更新
-        $match_connector = $this->connector_repository->create($request);
+            // 連絡者の検索と登録・更新
+            $match_connector = $this->connector_repository->create($request);
 
-        // 顧客の登録
-        $customer_id_list = $this->saveCustomer($request, $match_connector);
-        // 予約テーブル登録
-        $insert_reservation_id = $this->saveReservation($request, $reservation, $match_connector);
-       
-        // 担当講師（中間テーブル）の登録
-        $master_counts = $this->countMaster($request);
-        // 担当講師がいる場合、予約IDを紐づけ
-        if ($master_counts >= 1) {
-            for ($i = 1; $i <= $master_counts; $i++) {
-                ${'master_reservation_'.$i} = Master::matchMasterName($request, $i)->first();
-                $reservation->masters()->attach(${'master_reservation_'.$i}->id);
-            }
-        }
+            // 顧客の登録
+            $customer_id_list = $this->saveCustomer($request, $match_connector);
+            // 予約テーブル登録
+            $insert_reservation_id = $this->saveReservation($request, $reservation, $match_connector);
         
-        // 着付対象者（中間テーブル）の登録
-        $this->customer_reservation_repository->save($request, $insert_reservation_id, $customer_id_list);
+            // 担当講師（中間テーブル）の登録
+            $master_counts = $this->countMaster($request);
+            // 担当講師がいる場合、予約IDを紐づけ
+            if ($master_counts >= 1) {
+                for ($i = 1; $i <= $master_counts; $i++) {
+                    ${'master_reservation_'.$i} = Master::matchMasterName($request, $i)->first();
+                    $reservation->masters()->attach(${'master_reservation_'.$i}->id);
+                }
+            }
+            
+            // 着付対象者（中間テーブル）の登録
+            $this->customer_reservation_repository->save($request, $insert_reservation_id, $customer_id_list);
+        });
     }
 
     // 予約の編集
     public function edit($request, $id)
     {
-        $reservation = Reservation::find($id);
+        DB::transaction(function() use($request, $id) {
+            $reservation = Reservation::find($id);
 
-        // 連絡者の検索と登録・更新
-        $match_connector = $this->connector_repository->edit($request);
+            // 連絡者の検索と登録・更新
+            $match_connector = $this->connector_repository->edit($request);
 
-        // 顧客の登録
-        $customer_id_list = $this->saveCustomer($request, $match_connector);
-        // 予約テーブル登録
-        $insert_reservation_id = $this->saveReservation($request, $reservation, $match_connector);
-       
-        // 担当講師（中間テーブル）の登録
-        $master_counts = $this->countMaster($request);
-        // 担当講師がいる場合、予約IDを紐づけ
-        if ($master_counts >= 1) {
-            // 予約IDに紐づいた講師データを一度削除
-            $reservation->masters()->detach();
+            // 顧客の登録
+            $customer_id_list = $this->saveCustomer($request, $match_connector);
+            // 予約テーブル登録
+            $insert_reservation_id = $this->saveReservation($request, $reservation, $match_connector);
+        
+            // 担当講師（中間テーブル）の登録
+            $master_counts = $this->countMaster($request);
+            // 担当講師がいる場合、予約IDを紐づけ
+            if ($master_counts >= 1) {
+                // 予約IDに紐づいた講師データを一度削除
+                $reservation->masters()->detach();
 
-            // 予約IDと講師データを再度紐づけ
-            for ($i = 1; $i <= $master_counts; $i++) {
-                ${'master_reservation_'.$i} = Master::matchMasterName($request, $i)->first();
-                $reservation->masters()->attach(${'master_reservation_'.$i}->id);
+                // 予約IDと講師データを再度紐づけ
+                for ($i = 1; $i <= $master_counts; $i++) {
+                    ${'master_reservation_'.$i} = Master::matchMasterName($request, $i)->first();
+                    $reservation->masters()->attach(${'master_reservation_'.$i}->id);
+                }
             }
-        }
 
-        // 着付対象者（中間テーブル）の更新
-        // 予約IDに紐づいた顧客データを一度削除
-        $reservation->customers()->detach();
-        // 予約IDと顧客データを再度紐づけ
-        $this->customer_reservation_repository->save($request, $insert_reservation_id, $customer_id_list);
+            // 着付対象者（中間テーブル）の更新
+            // 予約IDに紐づいた顧客データを一度削除
+            $reservation->customers()->detach();
+            // 予約IDと顧客データを再度紐づけ
+            $this->customer_reservation_repository->save($request, $insert_reservation_id, $customer_id_list);
 
-        // 予約詳細画面へリダイレクトするため、インスタンスを返却
-        return $reservation;
+            // 予約詳細画面へリダイレクトするため、インスタンスを返却
+            return $reservation;
+        });
     }
 
     // 顧客の登録部分
