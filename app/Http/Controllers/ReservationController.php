9ea968c945206fada1c8e364d257021284b78b85
Erika Ito <itouerika21@gmail.com>
2019-09-20 17:01:00 +0900

    改善：　予約コントローラーの編集メソッドにファサードパターンを使用

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -4,15 +4,10 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use App\Connector;
-use App\Master;
 use App\Reservation;
 use Illuminate\Http\Request;
 use App\Http\Requests\ReservationRequest;
 use App\Services\ReservationService;
-use App\Repositories\ConnectorRepository;
-use App\Repositories\CustomerRepository;
-use App\Repositories\ReservationRepository;
-use App\Repositories\CustomerReservationRepository;
 
 class ReservationController extends Controller
 {
@@ -78,7 +73,7 @@ class ReservationController extends Controller
     // 新規登録処理
     public function create(ReservationRequest $request, ReservationService $service)
     {
-        $service->save($request);
+        $service->create($request);
 
         return redirect()->route('reservations.index');
     }
@@ -130,68 +125,9 @@ class ReservationController extends Controller
     }
     
     // 編集処理
-    public function edit(ReservationRequest $request, int $id, ConnectorRepository $connector_repository,
-        CustomerRepository $customer_repository, ReservationRepository $reservation_repository, CustomerReservationRepository $customer_reservation_repository)
+    public function edit(ReservationRequest $request, int $id, ReservationService $service)
     {
-        $reservation = Reservation::find($id);
-
-        // 連絡者テーブルの検索、更新
-        // 他テーブルに紐づけるため、連絡者を格納
-        $match_connector = $connector_repository->edit($request);
-        
-        // 顧客テーブルの登録・更新
-        // 顧客データの個数をカウント
-        $customer_name_list = [];
-        for ($i = 1; $i <= 3; $i++) {
-            if ($request->filled('name_'.$i)) {
-                $customer_name_list[] = 'name_'.$i;
-            }
-        }
-        $customer_counts = count($customer_name_list);
-
-        // 人数分の顧客データを更新
-        $customer_id_list = [];
-        for ($i = 1; $i <= $customer_counts; $i++) {
-            // 中間テーブル（着付対象者）登録に必要なため、customer_idを配列に格納
-            $customer_id_list[] = $customer_repository->save($request, $i, $match_connector);
-        }
-
-        // 予約テーブルの編集
-        // 中間テーブル登録に必要なため、reservation_idを格納
-        $insert_reservation_id = $reservation_repository->save($request, $reservation, $match_connector);
-
-        // 中間テーブル（担当講師）への保存・更新
-        // 担当講師データの個数をカウント
-        $master_name_list = [];
-        for ($i = 1; $i <= 4; $i++) {
-            if ($request->filled('master_'.$i)) {
-                $master_name_list[] = 'master_'.$i;
-            }
-        }
-
-        $master_counts = 0;
-        if (is_array($master_name_list)) {
-            $master_counts = count($master_name_list);
-        }
-
-        // 担当講師がいる場合、予約IDを紐づけ
-        if ($master_counts >= 1) {
-            // 予約IDに紐づいた講師データを一度削除
-            $reservation->masters()->detach();
-
-            // 予約IDと講師データを再度紐づけ
-            for ($i = 1; $i <= $master_counts; $i++) {
-                ${'master_reservation_'.$i} = Master::matchMasterName($request, $i)->first();
-                $reservation->masters()->attach(${'master_reservation_'.$i}->id);
-            }
-        }
-
-        // 中間テーブル（着付対象者）への保存・更新
-        // 予約IDに紐づいた顧客データを一度削除
-        $reservation->customers()->detach();
-
-        // 予約IDと顧客データを再度紐づけ
-        $customer_reservation_repository->save($request, $insert_reservation_id, $customer_id_list);
+        $reservation = $service->edit($request, $id);
 
         return redirect()->route('reservations.show', [
             'reservation' => $reservation,
