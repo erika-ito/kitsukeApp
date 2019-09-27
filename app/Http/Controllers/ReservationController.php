064b24aa12d9e6475634cb23c6dafdb2c0285830
vagrant <vagrant@homestead>
2019-09-27 16:15:47 +0000

    変更：　LOGファサード追加部分を削除

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -15,7 +15,6 @@ class ReservationController extends Controller
     public function index(Request $request)
     {
         $keyword = str_replace('/', '-', $request->keyword);
-        Log::debug($keyword);
         $pass_cancel = $request->pass_cancel;
 
         //　一覧表示のカラムを限定
