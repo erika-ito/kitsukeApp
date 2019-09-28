cadd2cbec01cacade094b64241158dbfc6fc76ca
Erika Ito <itouerika21@gmail.com>
2019-09-28 16:29:28 +0900

    変更：　フッターの内容をコピーライトに変更

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -41,21 +41,13 @@
     </main>
 
     <footer>
-        <div class="text-center">
-            <p>きもの学院</p>
+        <div class="text-right mr-5">
+            <p>&copy;2019 Erika Ito</p>
         </div>
     </footer>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     @yield('scripts')
-    <!-- <script>
-       $(function() {
-           $('ul.navbar-nav li').click(function() {
-               $('ul.navbar-nav li.active').removeClass('active');
-               $(this).addClass('active');
-           });
-       });
-   </script> -->
 </body>
 </html>
